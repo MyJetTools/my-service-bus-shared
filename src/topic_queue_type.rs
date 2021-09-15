f3b0e11c6a7acefa0c73135f158604c9c95a2027
Andrey <amigin@gmail.com>
2021-09-15 17:29:26 +0300

    Improved TopicQueueType

--- src/topic_queue_type.rs
+++ src/topic_queue_type.rs
@@ -4,3 +4,20 @@ pub enum TopicQueueType {
     DeleteOnDisconnect = 1,
     PermanentWithSingleConnection = 2,
 }
+
+impl Into<TopicQueueType> for u8 {
+    fn into(self) -> TopicQueueType {
+        match self {
+            0 => TopicQueueType::Permanent,
+            1 => TopicQueueType::DeleteOnDisconnect,
+            2 => TopicQueueType::PermanentWithSingleConnection,
+            _ => TopicQueueType::DeleteOnDisconnect,
+        }
+    }
+}
+
+impl Into<u8> for TopicQueueType {
+    fn into(self) -> u8 {
+        self as u8
+    }
+}

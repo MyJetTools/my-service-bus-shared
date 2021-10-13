d57e87053bc0227b8a72f541d38e5b9093c155fb
Andrey <amigin@gmail.com>
2021-10-13 19:54:49 +0300

    Simplified Messages Bucket

--- src/messages_bucket/messages_bucket.rs
+++ src/messages_bucket/messages_bucket.rs
@@ -1,103 +1,50 @@
-use std::{collections::BTreeMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc};
 
-use crate::{
-    messages_page::MessagesPage, page_id::PageId, queue_with_intervals::QueueWithIntervals,
-    MessageId,
-};
+use crate::{messages_page::MessagesPage, queue_with_intervals::QueueWithIntervals, MessageId};
 
-use super::MessagesBucketPage;
+use super::MessageToSendModel;
 
 pub struct MessagesBucket {
-    pub pages: BTreeMap<PageId, MessagesBucketPage>,
-    pub min_id: Option<MessageId>,
-    pub total_size: usize,
+    pub page: Arc<MessagesPage>,
+    pub messages: HashMap<MessageId, MessageToSendModel>,
+    pub messages_size: usize,
+    pub ids: QueueWithIntervals,
 }
 
 impl MessagesBucket {
-    pub fn new() -> Self {
+    pub fn new(page: Arc<MessagesPage>) -> Self {
         Self {
-            pages: BTreeMap::new(),
-            min_id: None,
-            total_size: 0,
+            page,
+            messages: HashMap::new(),
+            messages_size: 0,
+            ids: QueueWithIntervals::new(),
         }
     }
 
-    pub fn has_page(&self, page_id: PageId) -> bool {
-        self.pages.contains_key(&page_id)
-    }
+    pub fn add(&mut self, msg_id: MessageId, attempt_no: i32, msg_size: usize) {
+        let message = MessageToSendModel {
+            msg_id,
+            attempt_no,
+            msg_size,
+        };
 
-    pub fn add_page(&mut self, page: Arc<MessagesPage>) {
-        let page = MessagesBucketPage::new(page);
-        self.pages.insert(page.page.page_id, page);
-    }
+        self.messages.insert(message.msg_id, message);
+        self.messages_size += msg_size;
 
-    pub fn get_page(&mut self, page_id: PageId) -> &mut MessagesBucketPage {
-        return self.pages.get_mut(&page_id).unwrap();
+        self.ids.enqueue(msg_id);
     }
 
     pub fn messages_count(&self) -> usize {
-        let mut result = 0;
-
-        for page in self.pages.values() {
-            result += page.messages_count();
-        }
-
-        result
+        return self.messages.len();
     }
 
-    #[inline]
-    fn update_min_id(&mut self, msg_id: MessageId) {
-        if let Some(min_id) = self.min_id {
-            if min_id > msg_id {
-                self.min_id = Some(msg_id);
-            }
-        } else {
-            self.min_id = Some(msg_id)
-        }
-    }
+    pub fn remove(&mut self, message_id: MessageId) -> Option<MessageToSendModel> {
+        let removed_message = self.messages.remove(&message_id)?;
 
-    pub fn add_total_size(&mut self, msg_id: MessageId, msg_size: usize) {
-        self.total_size += msg_size;
+        self.messages_size -= removed_message.msg_size;
 
-        self.update_min_id(msg_id);
-    }
+        self.ids.remove(message_id);
 
-    pub fn remove_message(&mut self, page_id: PageId, msg_id: MessageId) -> bool {
-        let page = self.pages.get_mut(&page_id);
-
-        if page.is_none() {
-            return false;
-        }
-
-        let page = page.unwrap();
-
-        if page.page.page_id != page_id {
-            println!("Somehow we are here");
-            return false;
-        }
-
-        let removed = page.remove(msg_id);
-
-        if removed.is_none() {
-            return false;
-        }
-
-        let removed = removed.unwrap();
-
-        self.total_size -= removed.msg_size;
-
-        return true;
-    }
-
-    pub fn get_ids(&self) -> QueueWithIntervals {
-        let mut result = QueueWithIntervals::new();
-
-        for page in self.pages.values() {
-            for interval in &page.ids.intervals {
-                result.enqueue_range(interval);
-            }
-        }
-
-        result
+        return Some(removed_message);
     }
 }
--- src/messages_bucket/mod.rs
+++ src/messages_bucket/mod.rs
@@ -1,6 +1,4 @@
-mod bucket_page;
 mod message_to_send_model;
 mod messages_bucket;
-pub use bucket_page::MessagesBucketPage;
 pub use message_to_send_model::MessageToSendModel;
 pub use messages_bucket::MessagesBucket;

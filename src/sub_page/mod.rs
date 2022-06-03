554a10b33e9f7e6efb674da2d10a4271190e2e30
amigin <amigin@gmail.com>
2022-06-03 20:55:32 +0300

    Exposed MessageStatus

--- src/sub_page/mod.rs
+++ src/sub_page/mod.rs
@@ -1,6 +1,6 @@
 mod sub_page;
 mod sub_page_id;
 mod sub_page_messages_iterator;
-pub use sub_page::SubPage;
+pub use sub_page::{MessageStatus, SubPage};
 pub use sub_page_id::{SubPageId, SUB_PAGE_MESSAGES_AMOUNT};
 pub use sub_page_messages_iterator::SubPageMessagesIterator;

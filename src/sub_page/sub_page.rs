d246d95114cdd2e294ecc3548bca7ec350046902
amigin <amigin@gmail.com>
2022-06-04 10:01:08 +0300

    SubPage now can have optimized state - all messages are missing

--- src/sub_page/sub_page.rs
+++ src/sub_page/sub_page.rs
@@ -6,9 +6,14 @@ use crate::{MessageId, MySbMessage, MySbMessageContent};
 
 use super::SubPageId;
 
+pub enum SubPageMessages {
+    AllAreMissing,
+    Messages(BTreeMap<i64, MySbMessage>),
+}
+
 pub struct SubPage {
     pub sub_page_id: SubPageId,
-    pub messages: BTreeMap<i64, MySbMessage>,
+    pub messages: SubPageMessages,
     pub created: DateTimeAsMicroseconds,
     size: usize,
 }
@@ -17,7 +22,7 @@ impl SubPage {
     pub fn new(sub_page_id: SubPageId) -> Self {
         Self {
             sub_page_id,
-            messages: BTreeMap::new(),
+            messages: SubPageMessages::Messages(BTreeMap::new()),
             created: DateTimeAsMicroseconds::now(),
             size: 0,
         }
@@ -28,51 +33,79 @@ impl SubPage {
 
         Self {
             sub_page_id,
-            messages: messages,
+            messages: SubPageMessages::Messages(messages),
             created: DateTimeAsMicroseconds::now(),
             size,
         }
     }
 
+    pub fn create_with_all_missing(sub_page_id: SubPageId) -> Self {
+        Self {
+            sub_page_id,
+            messages: SubPageMessages::AllAreMissing,
+            created: DateTimeAsMicroseconds::now(),
+            size: 0,
+        }
+    }
+
     pub fn add_message(&mut self, message: MySbMessageContent) {
-        self.size += message.content.len();
-        if let Some(old_message) = self
-            .messages
-            .insert(message.id, MySbMessage::Loaded(message))
-        {
-            if let MySbMessage::Loaded(old_message) = old_message {
-                self.size -= old_message.content.len();
+        match &mut self.messages {
+            SubPageMessages::AllAreMissing => {
+                panic!("You can not insert message into sub page with all messages missing");
+            }
+            SubPageMessages::Messages(messages) => {
+                self.size += message.content.len();
+                if let Some(old_message) = messages.insert(message.id, MySbMessage::Loaded(message))
+                {
+                    if let MySbMessage::Loaded(old_message) = old_message {
+                        self.size -= old_message.content.len();
+                    }
+                }
             }
         }
     }
 
     pub fn add_messages(&mut self, new_messages: Vec<MySbMessageContent>) {
-        for message in new_messages {
-            self.size += message.content.len();
-            if let Some(old_message) = self
-                .messages
-                .insert(message.id, MySbMessage::Loaded(message))
-            {
-                if let MySbMessage::Loaded(old_message) = old_message {
-                    self.size -= old_message.content.len();
+        match &mut self.messages {
+            SubPageMessages::AllAreMissing => {
+                panic!("You can not insert message into sub page with all messages missing");
+            }
+            SubPageMessages::Messages(messages) => {
+                for message in new_messages {
+                    self.size += message.content.len();
+                    if let Some(old_message) =
+                        messages.insert(message.id, MySbMessage::Loaded(message))
+                    {
+                        if let MySbMessage::Loaded(old_message) = old_message {
+                            self.size -= old_message.content.len();
+                        }
+                    }
                 }
             }
         }
     }
 
     pub fn get_message(&self, message_id: MessageId) -> Option<&MySbMessage> {
-        self.messages.get(&message_id)
+        match &self.messages {
+            SubPageMessages::AllAreMissing => None,
+            SubPageMessages::Messages(messages) => messages.get(&message_id),
+        }
     }
     pub fn get_messages(&self, from_id: MessageId, to_id: MessageId) -> Option<Vec<&MySbMessage>> {
-        let mut result = LazyVec::new();
+        match &self.messages {
+            SubPageMessages::AllAreMissing => None,
+            SubPageMessages::Messages(messages) => {
+                let mut result = LazyVec::new();
 
-        for message_id in from_id..=to_id {
-            if let Some(msg) = self.messages.get(&message_id) {
-                result.add(msg);
+                for message_id in from_id..=to_id {
+                    if let Some(msg) = messages.get(&message_id) {
+                        result.add(msg);
+                    }
+                }
+
+                result.get_result()
             }
         }
-
-        result.get_result()
     }
 
     pub fn get_size(&self) -> usize {

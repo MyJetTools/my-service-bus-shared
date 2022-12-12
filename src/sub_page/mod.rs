cc22ebece8b420ea37d52d4a63359343e1ba783c
amigin <amigin@gmail.com>
2022-12-13 00:30:54 +0200

    Small fixes

--- src/sub_page/mod.rs
+++ src/sub_page/mod.rs
@@ -1,6 +1,6 @@
 mod size_and_amount;
 mod sub_page;
 mod sub_page_id;
-pub use size_and_amount::SizeAndAmount;
-pub use sub_page::SubPage;
+pub use size_and_amount::*;
+pub use sub_page::*;
 pub use sub_page_id::*;

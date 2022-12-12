61877f74127affaa7dec8610ebaae9e56376a410
amigin <amigin@gmail.com>
2022-12-13 00:35:47 +0200

    Small fixes

--- src/sub_page/sub_page.rs
+++ src/sub_page/sub_page.rs
@@ -7,26 +7,9 @@ use crate::messages::MySbMessageContent;
 
 use super::{SizeAndAmount, SubPageId};
 
-pub enum GetMessageResult<'s> {
-    Ok(&'s MySbMessageContent),
-    Missing(MessageId),
-    Gced(MessageId),
-}
-
-impl<'s> GetMessageResult<'s> {
-    pub fn unwrap_as_message(&self) -> &'s MySbMessageContent {
-        match self {
-            GetMessageResult::Ok(msg) => msg,
-            GetMessageResult::Missing(id) => panic!("Message {} is missing", id),
-            GetMessageResult::Gced(id) => panic!("Message {} is gced", id),
-        }
-    }
-}
-
 pub struct SubPage {
     pub sub_page_id: SubPageId,
     pub messages: BTreeMap<MessageId, MySbMessageContent>,
-    pub gced: QueueWithIntervals,
     pub to_persist: QueueWithIntervals,
     pub created: DateTimeAsMicroseconds,
     size_and_amount: SizeAndAmount,
@@ -39,7 +22,6 @@ impl SubPage {
             messages: BTreeMap::new(),
             created: DateTimeAsMicroseconds::now(),
             size_and_amount: SizeAndAmount::new(),
-            gced: QueueWithIntervals::new(),
             to_persist: QueueWithIntervals::new(),
         }
     }
@@ -59,7 +41,6 @@ impl SubPage {
             messages,
             created: DateTimeAsMicroseconds::now(),
             size_and_amount,
-            gced: QueueWithIntervals::new(),
             to_persist: QueueWithIntervals::new(),
         }
     }
@@ -76,15 +57,8 @@ impl SubPage {
         None
     }
 
-    pub fn get_message(&self, msg_id: MessageId) -> GetMessageResult {
-        if let Some(result) = self.messages.get(&msg_id) {
-            return GetMessageResult::Ok(result);
-        }
-        if self.gced.has_message(msg_id) {
-            return GetMessageResult::Gced(msg_id);
-        } else {
-            return GetMessageResult::Missing(msg_id);
-        }
+    pub fn get_message(&self, msg_id: MessageId) -> Option<&MySbMessageContent> {
+        self.messages.get(&msg_id)
     }
 
     pub fn get_size_and_amount(&self) -> &SizeAndAmount {
@@ -93,10 +67,6 @@ impl SubPage {
 
     pub fn get_messages_amount(&self) -> usize {
         self.messages.len()
-    }
-
-    pub fn has_gced_messages(&self) -> bool {
-        self.gced.len() > 0
     }
 
     fn get_first_message_id(&self) -> Option<MessageId> {
@@ -123,7 +93,6 @@ impl SubPage {
 
             if let Some(message) = self.messages.remove(&msg_id) {
                 self.size_and_amount.removed(message.content.len());
-                self.gced.enqueue(msg_id);
             }
         }
     }
